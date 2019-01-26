4a073c522ca12e20926eb01f394fbb1607198dad
Alex Areshnikov <alex.areshnikov@gmail.com>
2019-01-26 09:54:22 -0700

    Implement config update/delete values

--- services/resolvers/voltages_resolver.py
+++ services/resolvers/voltages_resolver.py
@@ -8,9 +8,7 @@ class VoltagesResolver:
 
     def resolve(self, payload, screen):
         bank_voltages = self._parse_payload(payload)
-
         bank = Bank(bank_voltages=bank_voltages, bank_number=1)
-
         screen.render_bank(bank)
 
         return bank_voltages
--- services/util/config.py
+++ services/util/config.py
@@ -5,13 +5,39 @@ import yaml
 class Config(object):
     _default_config = {
         "mqtt_host": "mustang.local",
-        "mqtt_port": 1883
+        "mqtt_port": 1883,
+        "display_setting_max_cell_voltage": 2.65
     }
 
-    def __init__(self):
+    def __init__(self, config_file_name="config.yml"):
+        file_path = os.path.dirname(os.path.realpath(__file__))
+        self._config_path = file_path.replace("services/util", config_file_name)
         self._config = self._default_config
         self._config.update(self._yaml_config())
         self._config_to_object()
+
+    def update(self, key, value):
+        if not key:
+            return
+
+        self._config[key] = value
+        self._config_to_yaml()
+        self._config_to_object()
+
+    def delete_key(self, key):
+        self._config.pop(key, None)
+        self._config_to_yaml()
+        try:
+            delattr(self, key)
+        except AttributeError as exc:
+            print(f"Attr {key} not found. Skipped.")
+
+    def _сonfig_to_yaml(self):
+        with open(self._config_path, 'w+') as stream:
+            try:
+                yaml.dump(self._config, stream, default_flow_style=False)
+            except yaml.YAMLError as exc:
+                print(exc)
 
     def _config_to_object(self):
         for a, b in self._config.items():
@@ -23,11 +49,9 @@ class Config(object):
 
     def _yaml_config(self):
         yaml_config = {}
-        file_path = os.path.dirname(os.path.realpath(__file__))
-        config_path = file_path.replace("services/util", "config.yaml")
 
         try:
-            with open(config_path, 'r') as stream:
+            with open(self._config_path, 'r') as stream:
                 try:
                     yaml_config = yaml.load(stream)
                 except yaml.YAMLError as exc:
